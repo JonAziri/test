813b8aea50a84ef6c66940ed3a11a5794531440c
JonAziri <ja32627@seeu.edu.mk>
2026-03-16 15:01:03 +0100

    initial commit

---
+++ index.js
@@ -0,0 +1,8 @@
+console.log("hello git")
+
+//git init - inicializmi i gjith repozitorit
+//git status - statusi i fajllave
+//git add. shton file per fazen staging 
+//git rm . largon file
+//git push -push per ne online repo
+
